44d7b6a63fbff2805c960f582bb1c11ef4cca794
André Bastos <andrebastos@MacBook-Pro.local>
2016-09-02 00:06:02 +0100

    Changes to be committed: 	modified:   EuroMillionsDraw.js 	modified:   EuroMillionsTwitterBot.js

--- EuroMillionsDraw.js
+++ EuroMillionsDraw.js
@@ -1,20 +1,20 @@
 var request = require('request')
 
 var options = {
-  method: 'get',
-  url: 'https://euromillions.p.mashape.com/ResultsService/FindLast',
-  json: true, // Use,If you are sending JSON data
-  headers: {
-    // Specify headers, If any
-    'X-Mashape-Key': 'IVaS9kmtSimshTpovHj6wXeTbyfAp1OeXCBjsngxP7bPEMnUPI'
-  }
+    method: 'get',
+    url: 'https://euromillions.p.mashape.com/ResultsService/FindLast',
+    json: true, // Use,If you are sending JSON data
+    headers: {
+        // Specify headers, If any
+        'X-Mashape-Key': 'IVaS9kmtSimshTpovHj6wXeTbyfAp1OeXCBjsngxP7bPEMnUPI'
+    }
 }
 
-request(options, function (err, res, body) {
-  if (err) {
-    console.log('Error :', err)
-    return
-  }
-  console.log(' Body :', body)
+request(options, function(err, res, body) {
+    if (err) {
+        console.log('Error :', err)
+        return
+    }
+    console.log(' Body :', body)
 
 });
--- EuroMillionsTwitterBot.js
+++ EuroMillionsTwitterBot.js
@@ -7,38 +7,38 @@ let Twitter = new TwitterPackage(secret);
 
 let connector = EuroMillionsDB('mongodb://localhost:27017/EuroMillionsDraw', 'EuroMillions')
 
-Twitter.stream('statuses/filter', {track: '#MakeMeRichEuromillions'}, function(stream) {
-  stream.on('data', function(tweet) {
-    console.log(tweet.text);
-    let key = EuroMillionsKey.generateKey()
-    let record = {
-    	user: "@" + tweet.user.screen_name,
-    	key: key
-    } 
-	let reply = {
-		text: "Hi " + tweet.user.name + 
-		", your euro millions key is: "+ 
-		key.numbers.toString() + 
-		" and your stars are: "+key.stars.toString(),
-		screen_name: tweet.user.screen_name  
-	}
-  	connector.update(record, tweet.user.screen_name).
-  		then((data) => {
-  			console.log(weet.user.screen_name + " got a chance to be filthy rich!")
-  		}, (err) => {
-  			console.log(err)
-  		});
-    Twitter.post('direct_messages/new', reply,  function(error, tweetReply, response){
-      //if we get an error print it out
-      if(error){
-        console.log(error);
-      }
-      //print the text of the tweet we sent out
-      console.log(tweetReply.text);
+Twitter.stream('statuses/filter', { track: '#MakeMeRichEuromillions' }, function(stream) {
+    stream.on('data', function(tweet) {
+        console.log(tweet.text);
+        let key = EuroMillionsKey.generateKey()
+        let record = {
+            user: "@" + tweet.user.screen_name,
+            key: key
+        }
+        let reply = {
+            text: "Hi " + tweet.user.name +
+                ", your euro millions key is: " +
+                key.numbers.toString() +
+                " and your stars are: " + key.stars.toString(),
+            screen_name: tweet.user.screen_name
+        }
+        connector.update(record, tweet.user.screen_name).
+        then((data) => {
+            console.log(weet.user.screen_name + " got a chance to be filthy rich!")
+        }, (err) => {
+            console.log(err)
+        });
+        Twitter.post('direct_messages/new', reply, function(error, tweetReply, response) {
+            //if we get an error print it out
+            if (error) {
+                console.log(error);
+            }
+            //print the text of the tweet we sent out
+            console.log(tweetReply.text);
+        });
     });
-  });
 
-  stream.on('error', function(error) {
-    console.log(error);
-  });
+    stream.on('error', function(error) {
+        console.log(error);
+    });
 });
