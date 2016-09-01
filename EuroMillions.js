f9edcf72da98fbcc57662217d9d11b5cbd8c2075
André Bastos <andrebastos@MacBook-Pro.local>
2016-09-01 23:59:16 +0100

    Initial commit
     Changes to be committed:
    	new file:   EuroMillions.js
    	new file:   EuroMillionsDB.js
    	new file:   EuroMillionsDraw.js
    	new file:   EuroMillionsTwitterBot.js

---
+++ EuroMillions.js
@@ -0,0 +1,18 @@
+
+var EuroMillions = {
+	generateKey: function(){
+		let key = {
+			numbers: Array.apply(0, {length: 5})
+				.map(function(elem){
+					return Math.floor(Math.random() * 50) + 1;
+				}),
+			stars: Array.apply(0, {length: 2})
+				.map(function(elem){
+					return Math.floor(Math.random() * 11) + 1;
+				})
+		}
+		return key
+	}
+}
+
+module.exports = EuroMillions
