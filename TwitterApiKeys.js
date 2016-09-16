5d26716285b0f84c7a5909c8bc2324d0e0b730da
André Bastos <andrebastos@MacBook-Pro.local>
2016-09-17 00:00:58 +0100

    Remove files

--- TwitterApiKeys.js
+++
@@ -1,8 +0,0 @@
-let secret = {
-    consumer_key: '6OS4nGfIHNzPzP8qtec2xV1JU',
-    consumer_secret: 't9oC9q1KTtjFTldD2xp4Vlrdk23TINMRq5XHGqgdEzqmV34e1s',
-    access_token_key: '73137353-wvcjjgiPWMcnH9qj9xz9ChOx7pW6DR2UpLWsZtx3F',
-    access_token_secret: 'bwdNWiDZYRvnB7cyx7QYPkifqqa8pJ1vXhQthMy14b8kl'
-}
-
-module.exports = EuroMillionsDB
