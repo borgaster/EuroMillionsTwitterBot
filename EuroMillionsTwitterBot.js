925880da66d63182249c9907d5718ed9073fe659
André Bastos <andrebastos@MacBook-Pro.local>
2016-09-21 21:39:21 +0100

    " style fix

--- EuroMillionsTwitterBot.js
+++ EuroMillionsTwitterBot.js
@@ -1,35 +1,35 @@
-let TwitterPackage = require('twitter');
-let EuroMillionsKey = require('./EuroMillions.js');
-let EuroMillionsDB = require('./EuroMillionsDB.js');
-let secret = require('./TwitterApiKeys.js');
+let TwitterPackage = require("twitter");
+let EuroMillionsKey = require("./EuroMillions.js");
+let EuroMillionsDB = require("./EuroMillionsDB.js");
+let secret = require("./TwitterApiKeys.js");
 
 let Twitter = new TwitterPackage(secret);
 
-let connector = EuroMillionsDB('mongodb://localhost:27017/EuroMillionsDraw', 'EuroMillions');
+let connector = EuroMillionsDB("mongodb://localhost:27017/EuroMillionsDraw", "EuroMillions");
 
-Twitter.stream('statuses/filter', { track: '#MakeMeRichEuromillions' }, function(stream) {
-    stream.on('data', function(tweet) {
+Twitter.stream("statuses/filter", { track: "#MakeMeRichEuromillions" }, function(stream) {
+    stream.on("data", function(tweet) {
         console.log(tweet.text);
         let key = EuroMillionsKey.generateKey();
         let record = {
             user: "@" + tweet.user.screen_name,
-            key: key
+            key: key,
+            checked: false
         };
         let reply = {
             text: "Hi " + tweet.user.name +
                 ", your euro millions key is: " +
                 key.numbers.toString() +
                 " and your stars are: " + key.stars.toString(),
-            screen_name: tweet.user.screen_name,
-            checked: false
+            screen_name: tweet.user.screen_name
         };
         connector.update(record, tweet.user.screen_name).
-        then((data) => {
+        then(() => {
             console.log(tweet.user.screen_name + " got a chance to be filthy rich!");
         }, (err) => {
             console.log(err);
         });
-        Twitter.post('direct_messages/new', reply, function(error, tweetReply, response) {
+        Twitter.post("direct_messages/new", reply, function(error, tweetReply, response) {
             //if we get an error print it out
             if (error) {
                 console.log(error);
@@ -39,7 +39,7 @@ Twitter.stream('statuses/filter', { track: '#MakeMeRichEuromillions' }, function
         });
     });
 
-    stream.on('error', function(error) {
+    stream.on("error", function(error) {
         console.log(error);
     });
 });
