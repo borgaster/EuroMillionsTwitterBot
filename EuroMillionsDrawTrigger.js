01b73d86edbfa6814f6aa77895139f28873fc02c
André Bastos <andrebastos@MacBook-Pro.local>
2016-10-02 22:26:38 +0100

    reduntant console log removed

--- EuroMillionsDrawTrigger.js
+++ EuroMillionsDrawTrigger.js
@@ -1,9 +1,14 @@
 let EuroMillionsDraw = require("./EuroMillionsDraw.js");
+let schedule = require("node-schedule");
 
-EuroMillionsDraw.fetchResults().then((result) => {
-    console.log("resolved");
-    console.log(result);
-}, (err) =>{
-    console.log("error");
-    console.log(err);
+let rule = new schedule.RecurrenceRule();
+rule.minute = new schedule.Range(0, 59, 1);
+schedule.scheduleJob(rule, function(){
+     EuroMillionsDraw.fetchResults().then((result) => {
+        console.log("resolved");
+        console.log(result);
+    }, (err) => {
+        console.log("error");
+        console.log(err);
+    });
 });
